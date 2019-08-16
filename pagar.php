d520aa2a9b32e74f8c3ee57f2c15afaa382f1f39
eliud <eliudlozautt@hotmail.com>
2019-08-16 08:11:34 -0500

    INSERCION DE DE COMPRA EN BASE DE DATOS LISTA

--- pagar.php
+++ pagar.php
@@ -3,33 +3,41 @@ include 'config.php';
 include 'conexion.php';
 include 'validarcart.php';
 ?>
-
 <?php
 echo "<script>alert('Producto comprado...');</script>";
 $pago=$_POST['pago'];
 echo $username = $_SESSION['username'];
+$user=$_SESSION['user'];
 if ($_POST) {
-      // $SID=session_id();
+
+
       $total=0;
-      // $orden=5;
       foreach($_SESSION['CARRITO'] as $indice=>$producto ){
       $total=$total+($producto['PRECIO']*$producto['CANTIDAD']);
-      // $idVENTA=$conexion->lastInsertId();
       }
-      $resultado=$conexion->prepare("INSERT INTO `orders` (`orderid`, `usersid`, `total`, `orderdate`, `orderstatus`, `id_payment`)
-      VALUES (NULL, '6', :total , NOW() , 'pendiente', :id_payment);");
+
+
+      // insertar datos de orden
+      $iduser=$user['id'];
+      $resultado=$conexion->prepare("INSERT INTO `orders` (`orderid`, `usersid`, `total`, `orderdate`, `orderstatus`, `id_payment`) VALUES (NULL, $iduser, :total , NOW() , 'pendiente', :id_payment);");
       $resultado->bindParam(":total",$total);
       $resultado->bindParam(":id_payment",$pago);
       $resultado->execute();
-      $idVENTA=$conexion->lastInsertId();
 
+      // insertar datos de pago
+      $idVENTA=$conexion->lastInsertId();
+      $resultado=$conexion->prepare("INSERT INTO `users_payments` (`id_cp`, `id_user`, `id_payment`) VALUES (NULL,$iduser ,$pago); ");
+      $resultado->execute();
+
+
+
+      // insertar datos de detalle de orden
       foreach($_SESSION['CARRITO'] as $indice=>$producto ){
       $id_producto=$producto['ID'];
       $precio=$producto['PRECIO'];
       $cantidad=$producto['CANTIDAD'];
-      $resultado=$conexion->prepare("INSERT INTO `orderdetails` (`orderid`, `productid`, `unitprice`, `quantity`, `discount`) 
-      VALUES (:orderid , :productid , :unitprice, :quantity , '0');");
-      
+      $resultado=$conexion->prepare("INSERT INTO `orderdetails` (`orderid`, `productid`, `unitprice`, `quantity`, `discount`) VALUES (:orderid , :productid , :unitprice, :quantity , '0');");
+
       $resultado->bindParam(":orderid",$idVENTA);
       $resultado->bindParam(":productid",$id_producto);
       $resultado->bindParam(":unitprice",$precio);
@@ -37,5 +45,4 @@ if ($_POST) {
       $resultado->execute();
     }
 }
-
 ?>
