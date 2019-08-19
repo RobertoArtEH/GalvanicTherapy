fa29527b2929c2105daa97e1690a5ea146420058
Andres Orozco <madrid_koz@hotmail.com>
2019-08-19 03:06:10 -0500

    consultas

--- validar-categorias.php
+++ validar-categorias.php
@@ -6,13 +6,13 @@ require('conexion.php');
     $resultado= $consulta->fetchAll(PDO::FETCH_ASSOC);
 
 //tendencias
-    $sqltrends = $conexion->prepare('SELECT * from orders left join orderdetails on orders.orderid=orderdetails.orderid left join products on orderDetails.productid =
-    products.productid left join categories on products.categoryid = categories.categoryid where unitsinstock >0 group by products.productname order by orders.orderid desc');
+    $sqltrends = $conexion->prepare('SELECT * from orders left join orderdetails on orders.orderid=orderdetails.orderid left join products on orderdetails.productid =
+    products.productid left join categories on products.categoryid = categories.categoryid where unitsinstock >0 group by products.productname order by orders.orderid desc LIMIT 0,9');
     $sqltrends -> execute();
     $tendencias= $sqltrends->fetchAll(PDO::FETCH_ASSOC);
 
 //lonuevo
-    $sqlnew = $conexion->prepare('SELECT * from products where unitsinstock >0 order by productid desc');
+    $sqlnew = $conexion->prepare('SELECT * from products where unitsinstock >0 order by productid desc LIMIT 0,9');
     $sqlnew-> execute();
     $new= $sqlnew->fetchAll(PDO::FETCH_ASSOC);
       
